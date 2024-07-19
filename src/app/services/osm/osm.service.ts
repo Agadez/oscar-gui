31b1bedb9c836b484041a19f4c8d1114781c95ff
Agadez <n.ratnamaheson@yahoo.de>
2024-07-20 01:19:02 +0200

    angular 18 update

--- src/app/services/osm/osm.service.ts
+++ src/app/services/osm/osm.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {TagSuggestion} from '../../models/osm/tag-suggestion';
 import {Observable} from 'rxjs';
 
